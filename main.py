31c394ed2fecebbb05500bac85451b92bdc38110
Sam Platt <platts1@sou.edu>
2023-03-28 11:12:04 -0700

    mostly works

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-import PySimpleGUI as sg
-from transformers import pipeline
+import argparse
 import os
+import PySimpleGUI as sg
+import sys
+
+from transformers import pipeline
 
 def setup():
+    if len(sys.argv) <= 1:
+        print("User has not specified an operating system. Please rerun program as:\tpython3 main.py [linux / mac / windows]\n")
+        exit()
+    operatingsystem = sys.argv[1]
+    match operatingsystem:
+        case "linux":
+            # install linux version of pytorch
+            os.system("pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu")
+        case "mac":
+            # install mac version of pytorch
+            os.system("pip3 install torch torchvision torchaudio")
+        case "windows":
+            # install windows version of pytorch
+            print("pip3 install torch torchvision torchaudio")
+        case _:
+            exit("Operating system not recognized. Please rerun program as:\tpython3 main.py [linux / mac / windows]\n")
+
     print("Installing required packages...")
     os.system("pip3 install -r requirements.txt")
 
+def textgeneration(prompt):
+    if len(prompt) == 0:
+        exit("A prompt was not entered.")
+    classifier = pipeline("text-generation")
+    result = str(classifier(prompt, max_length=250)[0]['generated_text'])
+    txt = "{0}\n\n{1}"
+    return txt.format(prompt,result)
+
 def app():
-    # All the stuff inside your window.
-    layout = [  [sg.Text('Some text on Row 1')],
-                [sg.Text('Filename:'), sg.InputText()],
+    # All the stuff inside your window
+    layout = [  [sg.Text('some text')],
+                [sg.Text('Prompt:'), sg.InputText()],
                 [sg.Button('Generate'), sg.Button('Cancel')] ]
 
     # Create the Window
-    window = sg.Window('Window Title', layout)
+    window = sg.Window('Shakespeare Alternate Endings', layout)
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
-        print('You entered ', values[0]) # prints what the user entered to the console
+        displaytext = textgeneration(values[0])
+        print(displaytext) # prints what the user entered to the console
 
     window.close()
 
