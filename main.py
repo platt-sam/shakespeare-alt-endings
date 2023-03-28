b1aaf5b244b29957682119222528c63bbe194cb3
Sam Platt <platts1@sou.edu>
2023-03-28 14:58:05 -0700

    complete

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
 import PySimpleGUI as sg
 import sys
-
+from datetime import date, datetime
+from os import path
 from transformers import pipeline
+
+plays = {
+    "Hamlet":"hamlet.txt",
+    "King Lear":"king_lear.txt",
+    "Macbeth":"macbeth.txt",
+    "Romeo and Juliet":"romeo_and_juliet.txt",
+    "Twelfth Night":"twelfth_night.txt",
+}
 
 def setup():
     if len(sys.argv) <= 1:
@@ -25,32 +34,52 @@ def setup():
     print("Installing required packages...")
     os.system("pip3 install -r requirements.txt")
 
-def textgeneration(prompt):
-    if len(prompt) == 0: # Ensure a prompt was entered
+def textgeneration(name):
+    if len(name) == 0: # Ensure a prompt was entered
         exit("A prompt was not entered.")
+    
+    filename = plays[name]
+    f = open("scripts/" + filename, "r")
+    prompt = f.read()
+    f.close()
 
     if len(prompt) > 1024: # Ensure input length is less than the maximum accepted by model
         prompt = prompt[0:1023]
 
     classifier = pipeline("text-generation")
     result = str(classifier(prompt, max_length=1024)[0]['generated_text'])
-    return result
+
+    fname = ('alternate ending_' + name + '_' + str(date.today()) + '_' + datetime.now().strftime('%H-%M-%S') + '.txt')
+    outputfile = open(fname, "w")
+    outputfile.write(result)
+    outputfile.close()
+
+    return result, fname
 
 def app():
     # All the stuff inside your window
-    layout = [  [sg.Text('Choose a play for the AI to finish for you')],
-                [sg.Text('Play:'), sg.Combo(["Hamlet", "King Lear", "Macbeth", "Romeo and Juliet", "Twelfth Night"], expand_x=True, enable_events=True,  readonly=False, key='-PLAYS-')],
-                [sg.Button('Generate'), sg.Button('Cancel')] ]
+    playnames = list(plays.keys())
+
+    layoutcontent = [
+        [sg.Text('Choose a Play:'), sg.Combo(playnames, enable_events=True, readonly=False, key='-PLAYS-')],
+        [sg.Text('', key='-RESULT-')],
+        #[sg.Column(layout='', key='-RESULT-', size=(300,None), scrollable=True)],
+    ]
 
     # Create the Window
-    window = sg.Window('Shakespeare Alternate Ending Generator', layout)
+    window = sg.Window(title='Shakespeare Alternate Ending Generator', layout=layoutcontent, size=(400,100))
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
-        prompt = values['-PLAYS-']
-        print(textgeneration(prompt))
+        playname = values['-PLAYS-']
+
+        res = textgeneration(playname)
+        generatedresult = res[0]
+        filename = res[1]
+        
+        window['-RESULT-'].update("Results saved to " + filename)
 
     window.close()
 
