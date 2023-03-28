c0b8014d126990788ee9b7caf208a335344f73ab
Sam Platt <platts1@sou.edu>
2023-03-28 13:16:06 -0700

    debugging, cleanup

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import PySimpleGUI as sg
 import sys
@@ -29,28 +28,29 @@ def setup():
 def textgeneration(prompt):
     if len(prompt) == 0: # Ensure a prompt was entered
         exit("A prompt was not entered.")
+
     if len(prompt) > 1024: # Ensure input length is less than the maximum accepted by model
         prompt = prompt[0:1023]
+
     classifier = pipeline("text-generation")
     result = str(classifier(prompt, max_length=1024)[0]['generated_text'])
-    txt = "{0}\n\n{1}"
-    return txt.format(prompt,result)
+    return result
 
 def app():
     # All the stuff inside your window
-    layout = [  [sg.Text('some text')],
-                [sg.Text('Prompt:'), sg.InputText()],
+    layout = [  [sg.Text('Choose a play for the AI to finish for you')],
+                [sg.Text('Play:'), sg.Combo(["Hamlet", "King Lear", "Macbeth", "Romeo and Juliet", "Twelfth Night"], expand_x=True, enable_events=True,  readonly=False, key='-PLAYS-')],
                 [sg.Button('Generate'), sg.Button('Cancel')] ]
 
     # Create the Window
-    window = sg.Window('Shakespeare Alternate Endings', layout)
+    window = sg.Window('Shakespeare Alternate Ending Generator', layout)
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
             break
-        displaytext = textgeneration(values[0])
-        print(displaytext) # prints what the user entered to the console
+        prompt = values['-PLAYS-']
+        print(textgeneration(prompt))
 
     window.close()
 
