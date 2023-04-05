bba7a206ba9f98c057ca6c98aafe47e1bfab9bba
Sam Platt <platts1@sou.edu>
2023-04-05 10:40:40 -0700

    changed how requirements install works

--- main.py
+++ main.py
@@ -8,21 +8,16 @@ def setup():
         print("User has not specified an operating system. Please rerun program as:\tpython3 main.py [linux / mac / windows]\n")
         exit()
     operatingsystem = sys.argv[1]
-    match operatingsystem:
-        case "linux":
-            # install linux version of pytorch
-            os.system("pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu")
-        case "mac":
-            # install mac version of pytorch
-            os.system("pip3 install torch torchvision torchaudio")
-        case "windows":
-            # install windows version of pytorch
-            print("pip3 install torch torchvision torchaudio")
-        case _:
-            exit("Operating system not recognized. Please rerun program as:\tpython3 main.py [linux / mac / windows]\n")
-
+    cmd = ""
+    if operatingsystem == 'linux':
+        cmd = "pip3 install -r requirements-linux.txt"
+    elif operatingsystem in ['mac', 'windows']:
+        cmd = "pip3 install -r requirements.txt"
+    else:
+        print("Operating System not recognized")
+        exit()
     print("Installing required packages...")
-    os.system("pip3 install -r requirements.txt")
+    os.system(cmd)
 
 def main():
     setup()
